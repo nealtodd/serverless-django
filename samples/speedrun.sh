d9cc7a25b330cfcdcae2f7ba1ca11fa3dbb49432
Neal Todd <neal.todd@torchbox.com>
2019-03-24 19:44:25 +0000

    Initial Speedrun docs

---
+++ samples/speedrun.sh
@@ -0,0 +1,68 @@
+# See nealtodd.github.io/serverless-django/djconeu-speedrun
+
+[[ ! -z "$SITE" ]] || exit
+
+mkdir ${SITE} && cd ${SITE}
+
+python3 -m venv ~/.venvs/${SITE}-env
+source ~/.venvs/${SITE}-env/bin/activate
+pip install --upgrade pip
+
+pip install zappa zappa-django-utils django-storages
+
+git clone https://github.com/wagtail/bakerydemo.git
+cd bakerydemo
+pip install -r requirements/base.txt
+
+cat << EOF >> bakerydemo/settings/dev.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'zappa_django_utils.db.backends.s3sqlite',
+        'NAME': '${SITE}-sqlite.db',
+        'BUCKET': '${SITE}-db'
+    }
+}
+
+ALLOWED_HOSTS = ['.eu-west-2.amazonaws.com', '.bygge.net']
+
+AWS_STORAGE_BUCKET_NAME = '${SITE}-static'
+INSTALLED_APPS += ('storages',)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+EOF
+
+cat << EOF > zappa_settings.json
+{
+    "dev": {
+        "django_settings": "bakerydemo.settings.dev",
+        "profile_name": "zappa",
+        "project_name": "${SITE}",
+        "runtime": "python3.6",
+        "s3_bucket": "${SITE}-zappa",
+        "aws_region": "eu-west-2",
+        "timeout_seconds": 60,
+        "domain": "${SITE}.bygge.net",
+        "certificate_arn": "${CERT_ARN}",
+        "aws_environment_variables": {
+            "DEBUG": "false"
+    	}
+    }
+}
+EOF
+
+aws s3api create-bucket --bucket ${SITE}-db --profile zappa \
+	--region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+aws s3api create-bucket --bucket ${SITE}-static --profile zappa \
+	--region eu-west-2 --create-bucket-configuration LocationConstraint=eu-west-2
+aws s3api put-bucket-cors --bucket ${SITE}-static --profile zappa --cors-configuration \
+	'{"CORSRules": [{"AllowedOrigins": ["*"], "AllowedMethods": ["GET"]}]}'
+aws s3api put-bucket-policy --bucket ${SITE}-static --profile zappa --policy \
+	'{"Statement": [{"Effect": "Allow","Principal": "*","Action": "s3:GetObject","Resource": "arn:aws:s3:::'${SITE}'-static/*"}]}'
+aws s3 sync bakerydemo/media/original_images/ s3://${SITE}-static/original_images/ --profile zappa
+
+zappa deploy dev
+zappa certify dev -y
+zappa manage dev "collectstatic --noinput"
+zappa manage dev migrate
+zappa manage dev load_initial_data
+zappa status dev
